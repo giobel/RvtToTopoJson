90df3f09f07f83911c2763795f95563dd56924ff
GLOBAL\Giovanni.Brogiolo <giovanni.brogiolo>
2020-04-14 09:17:25 +1000

    add try and catch statement

--- RvtToTopoJson/GeoJson.cs
+++ RvtToTopoJson/GeoJson.cs
@@ -32,118 +32,100 @@ namespace RvtToTopoJson
             UIDocument uidoc = uiApp.ActiveUIDocument;
             doc = uidoc.Document;
 
-
-            Transform ttr = doc.ActiveProjectLocation.GetTotalTransform().Inverse;
-            Transform projectlocationTransform = GetProjectLocationTransform(doc);
-
-            IList<Reference> roomReferences = uidoc.Selection.PickObjects(ObjectType.Element, "Select some rooms");
-
-            SpatialElementBoundaryOptions opt = new SpatialElementBoundaryOptions();
-
-            //control the boundary location
-            opt.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Center;
-
-            var features = new List<Feature>();
-
-            foreach (Reference re in roomReferences)
+            try
             {
 
-                Element roomElement = doc.GetElement(re);
+                Transform ttr = doc.ActiveProjectLocation.GetTotalTransform().Inverse;
+                Transform projectlocationTransform = GetProjectLocationTransform(doc);
 
-                string roomName = roomElement.LookupParameter("Name").AsString();
+                IList<Reference> roomReferences = uidoc.Selection.PickObjects(ObjectType.Element, "Select some rooms");
 
-                SpatialElement se = doc.GetElement(re) as SpatialElement;
+                SpatialElementBoundaryOptions opt = new SpatialElementBoundaryOptions();
 
-                //multiPolygon.Add(Helpers.RoomBoundary(doc, re, ttr));
-                
-                IList<IList<BoundarySegment>> loops = se.GetBoundarySegments(opt);
-                
-                var featureProps = new Dictionary<string, object> { { "Name", roomName }, { "Area", se.Area } };
+                //control the boundary location
+                opt.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Center;
 
-                var coordinates = new List<Position>();
+                var features = new List<Feature>();
 
-                foreach (IList<BoundarySegment> loop in loops)
+                foreach (Reference re in roomReferences)
                 {
-                    ElementId segId = new ElementId(123456);
 
-                    foreach (BoundarySegment seg in loop)
+                    Element roomElement = doc.GetElement(re);
+
+                    string roomName = roomElement.LookupParameter("Name").AsString();
+
+                    SpatialElement se = doc.GetElement(re) as SpatialElement;
+
+                    //multiPolygon.Add(Helpers.RoomBoundary(doc, re, ttr));
+
+                    IList<IList<BoundarySegment>> loops = se.GetBoundarySegments(opt);
+
+                    var featureProps = new Dictionary<string, object> { { "Name", roomName }, { "Area", se.Area } };
+
+                    var coordinates = new List<Position>();
+
+                    foreach (IList<BoundarySegment> loop in loops)
                     {
-                        Line segLine = seg.GetCurve() as Line;
-                        XYZ endPt = segLine.GetEndPoint(0);
+                        ElementId segId = new ElementId(123456);
 
-                        if (segId == seg.ElementId)
+                        foreach (BoundarySegment seg in loop)
                         {
+                            Line segLine = seg.GetCurve() as Line;
+                            XYZ endPt = segLine.GetEndPoint(0);
+
+                            if (segId == seg.ElementId)
+                            {
+
+                            }
+                            else
+                            {
+                                //TaskDialog.Show("re", $"{endPt.Y} {endPt.X}");
+                                coordinates.Add(new Position(endPt.Y * 0.3048, endPt.X * 0.3048));
+                            }
+
+                            segId = seg.ElementId;
 
                         }
-                        else
-                        {
-                            //TaskDialog.Show("re", $"{endPt.Y} {endPt.X}");
-                           coordinates.Add(new Position(endPt.Y*0.3048, endPt.X*0.3048));
-                        }
-
-                        segId = seg.ElementId;
-
                     }
+
+                    coordinates.Add(coordinates.First());
+
+                    var polygon = new Polygon(new List<LineString> { new LineString(coordinates) });
+
+                    features.Add(new Feature(polygon, featureProps));
+
                 }
 
-                coordinates.Add(coordinates.First());
 
-                var polygon = new Polygon(new List<LineString> { new LineString(coordinates) });
+                var models = new FeatureCollection(features);
 
-                features.Add(new Feature(polygon, featureProps));
 
+
+                #region File Export
+                //EXPORT GEOJSON
+                var serializedData = JsonConvert.SerializeObject(models, Formatting.Indented);
+                StreamWriter writetext = new StreamWriter(@"C:\Temp\export.json");
+                writetext.WriteLine(serializedData);
+                writetext.Flush();
+                writetext.Close();
+
+                #endregion
+                //Activate command in revit
+                #region Revit Start Command
+                Transaction trans = new Transaction(doc);
+                trans.Start("GeoReader");
+                trans.Commit();
+                TaskDialog.Show("File was built", "Success");
+                #endregion
+
+
+                return Result.Succeeded;
             }
-
-            //// x-y coords swapped (lat-long)
-            //var coordinates1 = new List<Position>
-            //    {
-            //        new Position(0,0),
-            //        new Position(0,1),
-            //        new Position(2,1),
-            //        new Position(2,0),
-            //        new Position(0,0)
-
-            //    }.ToList<IPosition>();
-
-            //var polygon1 = new Polygon(new List<LineString> { new LineString(coordinates1) });
-            //var featureProperties1 = new Dictionary<string, object> { { "Name", "Foo" },{ "Area", "456" } };
-            //var model1 = new Feature(polygon1, featureProperties1);
-
-            //var coordinates2 = new List<Position>
-            //    {
-            //        new Position(0,2),
-            //        new Position(0,3),
-            //        new Position(2,3),
-            //        new Position(2,2),
-            //        new Position(0,2)
-
-            //    }.ToList<IPosition>();
-
-            //var polygon2 = new Polygon(new List<LineString> { new LineString(coordinates2) });
-            //var featureProperties2 = new Dictionary<string, object> { { "Name", "Boo" },{ "Area", "123" } };
-            //var model2 = new GeoJSON.Net.Feature.Feature(polygon2, featureProperties2);
-
-
-
-            var models = new FeatureCollection(features);
-
-            #region File Export
-            //EXPORT GEOJSON
-            var serializedData = JsonConvert.SerializeObject(models, Formatting.Indented);
-            StreamWriter writetext = new StreamWriter(@"C:\Temp\export.json");
-            writetext.WriteLine(serializedData);
-            writetext.Flush();
-            writetext.Close();
-
-            #endregion
-            //Activate command in revit
-            #region Revit Start Command
-            Transaction trans = new Transaction(doc);
-            trans.Start("GeoReader");
-            trans.Commit();
-            TaskDialog.Show("File was built", "Success");
-            #endregion
-            return Result.Succeeded;
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Error", ex.Message);
+                return Result.Failed;
+            }
         }
 
 
